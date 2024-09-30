f99724b76bf9db9e16f04e4036bd5ab93f2f42d4
JasminNicola <130758335+JasminNicola@users.noreply.github.com>
2024-09-30 11:42:29 +0200

    TODO 1-4

--- src/main/java/cat/tecnocampus/fgcstations/persistence/StationRepository.java
+++ src/main/java/cat/tecnocampus/fgcstations/persistence/StationRepository.java
@@ -10,4 +10,17 @@ import java.util.List;
 import java.util.Optional;
 
 public interface StationRepository extends JpaRepository<Station, String> {
+    // @Query("SELECT s.name as name, s.longitud  as longitud, s.latitud as latitud  from Station s")
+    @Query("""
+            SELECT s From Station s
+            """)
+    List<StationDTO> findAllStationDTO();
+
+
+    Optional<Station> findByName(String name);
+
+    @Query("""
+            SELECT s From Station s WHERE s.name=:name
+            """)
+    Optional<StationDTO> findbyNameDTO(String name);
 }
